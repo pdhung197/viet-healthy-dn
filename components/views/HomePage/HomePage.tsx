22d56f03d14a8b238c701987b833a979017ed073
pdhung197 <pdhung.info@gmail.com>
2021-02-24 08:07:35 +0700

    Add slick

--- components/views/HomePage/HomePage.tsx
+++ components/views/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import {HomeProps} from "../../../models/PageProps";
+import { HomeCarousel } from "../../blocks/Carousels/HomeCarousel";
 
 export const HomePage = (props: HomeProps) => {
   const {products} = props;
   console.log({products});
   return (
     <div>
-      Home Page
+      <HomeCarousel/>
       <br />
       <Link href="/admin">
         <a>Admin</a>
